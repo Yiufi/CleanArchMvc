c5bbb721a6e3bdd804c763f54fc0c2c5f921b66a
Diego <105529723+Yiufi@users.noreply.github.com>
2022-05-24 13:04:42 -0300

    CQRS

--- CleanArchMvc/CleanArchMvc.Application/Interfaces/ICategoryService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -1,8 +1,5 @@
 ﻿using CleanArchMvc.Application.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.Interfaces
@@ -10,12 +7,9 @@ namespace CleanArchMvc.Application.Interfaces
     public interface ICategoryService
     {
         Task<IEnumerable<CategoryDTO>> GetCategories();
-        Task<CategoryDTO> GetById(int? Id);
-
-        Task Add(CategoryDTO categoryDTO);
-
-        Task Update(CategoryDTO categoryDTO);
-
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
         Task Remove(int? id);
     }
 }
--- CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -1,8 +1,5 @@
 ﻿using CleanArchMvc.Application.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.Interfaces
@@ -10,13 +7,11 @@ namespace CleanArchMvc.Application.Interfaces
     public interface IProductService
     {
         Task<IEnumerable<ProductDTO>> GetProductsAsync();
-        Task<ProductDTO> GetById(int? id);
+        //Task<ProductDTO> GetById(int? id);
 
-        Task<ProductDTO> GetProductCategory(int? id);
-
-        Task Add(ProductDTO productDTO);
-
-        Task Update(ProductDTO productDTO);
-        Task Remove(int? id);
+        //Task<ProductDTO> GetProductCategory(int? id);
+        //Task Add(ProductDTO productDto);
+       // Task Update(ProductDTO productDto);
+       // Task Remove(int? id);
     }
 }
