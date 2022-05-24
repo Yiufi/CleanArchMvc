b08d0b5db8f820b70d18a961eb229991f6de65b3
Diego <105529723+Yiufi@users.noreply.github.com>
2022-05-23 21:02:09 -0300

    Creating Entities

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }  
+    }
+}
