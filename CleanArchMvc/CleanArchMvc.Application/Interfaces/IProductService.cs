b9926cf219a973089f14aeb7b104f69b56f41d19
Diego <105529723+Yiufi@users.noreply.github.com>
2022-05-24 13:32:13 -0300

    Revert "Implementing WebUi Layer"
    This reverts commit ab61a8904da9b0443b9068e3d621d5a862878970.

--- CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -1,31 +1,17 @@
 ﻿using CleanArchMvc.Application.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.Interfaces
 {
     public interface IProductService
     {
-<<<<<<< HEAD
         Task<IEnumerable<ProductDTO>> GetProducts();
         Task<ProductDTO> GetById(int? id);
 
         //Task<ProductDTO> GetProductCategory(int? id);
         Task Add(ProductDTO productDto);
         Task Update(ProductDTO productDto);
-=======
-        Task<IEnumerable<ProductDTO>> GetProductsAsync();
-        Task<ProductDTO> GetById(int? id);
-
-        Task<ProductDTO> GetProductCategory(int? id);
-
-        Task Add(ProductDTO productDTO);
-
-        Task Update(ProductDTO productDTO);
->>>>>>> parent of c5bbb72 (CQRS)
         Task Remove(int? id);
     }
 }
--- CleanArchMvc/CleanArchMvc.Application/Services/CategoryService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Services/CategoryService.cs
@@ -3,10 +3,7 @@ using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Domain.Entities;
 using CleanArchMvc.Domain.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.Services
@@ -18,7 +15,7 @@ namespace CleanArchMvc.Application.Services
         public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
         {
             _categoryRepository = categoryRepository;
-            _mapper = mapper;   
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<CategoryDTO>> GetCategories()
@@ -27,21 +24,21 @@ namespace CleanArchMvc.Application.Services
             return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
         }
 
-        public async Task<CategoryDTO> GetById(int? Id)
+        public async Task<CategoryDTO> GetById(int? id)
         {
-            var categoryEntity = await _categoryRepository.GetById(Id);
+            var categoryEntity = await _categoryRepository.GetById(id);
             return _mapper.Map<CategoryDTO>(categoryEntity);
         }
 
-        public async Task Add(CategoryDTO categoryDTO)
+        public async Task Add(CategoryDTO categoryDto)
         {
-            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
             await _categoryRepository.Create(categoryEntity);
         }
 
-        public async Task Update(CategoryDTO categoryDTO)
+        public async Task Update(CategoryDTO categoryDto)
         {
-            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
             await _categoryRepository.Update(categoryEntity);
         }
 
